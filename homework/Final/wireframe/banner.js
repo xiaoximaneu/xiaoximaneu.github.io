5b525b519f5cead30e7d3fafeba0a8ed272b01ce
xiaoximaneu <ma.xiaoxi1@northeastern.edu>
2022-04-11 15:43:30 -0400

    hehe

---
+++ homework/Final/wireframe/banner.js
@@ -0,0 +1,13 @@
+
+var bannerPosition = d3.scaleLinear()
+.domain ([ 0, window.innerHeight])
+.range([100, 0]);
+
+d3.select(window)
+.on("scroll", function() {
+
+  var y = bannerPosition(window.scrollY);
+  d3.select("#banner")
+    .style("background-position", "50% " + y + "%");
+
+});
---
+++ homework/Final/wireframe/loaddata.js
@@ -0,0 +1,14 @@
+d3.csv("female_chess_player - standard_top.csv")
+   .then(function(csvData){
+
+    csvData.forEach(function (d){
+        d.Rank = parseFloat(d.Rank);
+        d.World_Rank = parseFloat(d.World_Rank);
+        d.Standard_Rating = parseFloat(d.Standard_Rating);
+        d.Year_of_birth = parseFloat(d.Year_of_birth);
+        d.Games = parseFloat(d.Games);
+    });
+
+    console.log(csvData);
+});
+
